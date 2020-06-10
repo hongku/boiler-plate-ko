143b39d80e6f512b0fb9ccb490940a32e9d07a57
hongsun <hongsun3152@gmail.com>
2020-06-10 21:21:27 +0900

    Update NODE ENV

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
+const config = require('./config/key');
 const app = express();
 const port = 5000;
 
-mongoose.connect('mongodb+srv://hongsun:qwer1234@boilerplate-pv8ar.mongodb.net/<dbname>?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-}).then(() => console.log('MongoDB Connected.. '))
-  .catch((err) => console.log(err))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected.. '))
+  .catch((err) => console.log(err));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// 회원가입에 필요한 정보를 Client에서 전달받아 DB에 넣는다.
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
